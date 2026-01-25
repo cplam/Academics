b817b8e8ef8149340a591d920a2346e54fb7f1b4
cplam <ballball231@gmail.com>
2026-01-26 02:31:14 +0800

    change rough_table.py

--- ELEC/ELEC3200--System Modeling, Analysis and Control/Programs/rough_table.py
+++ ELEC/ELEC3200--System Modeling, Analysis and Control/Programs/rough_table.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-a_s = np.array([2,3,5,6,5,7])
+# This is the input characteristic polynomial coefficients array
+# For example, the polynomial is 3s^4 + 3s^3 + 9s^2 + 3s + 5
+# If some coefficients are symbolic, please let the symbolic coefficients be 1000
+a_s = np.array([3,3,9,3,5])
 row = len(a_s)
 col = row//2 + row%2
 
@@ -15,6 +18,9 @@ Rough_table[0:2,:] = a_s
 for i in range(2,row):
     for j in range(col-1):
         Rough_table[i][j] = Rough_table[i-2][0] * Rough_table[i-1][j+1] - Rough_table[i-2][j+1] * Rough_table[i-1][0]
+        print(f"Calculating Rough_table[{i}][{j}]:")
+        print(f"numerator: {-Rough_table[i][j]}")
+        print(f"denominator: {Rough_table[i-1][0]}")
         if Rough_table[i-1][0] != 0:
             Rough_table[i][j] /=  (-Rough_table[i-1][0])
 
